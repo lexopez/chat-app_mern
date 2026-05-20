db4ab67564024e8d67f418da0ea09029af4078cc
alexander lopez <iamlexlopez@gmail.com>
2026-05-20 18:36:20 +0800

    update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api/v1"
-      : "/api/v1",
+      : `${import.meta.env.VITE_BASE_URL}/api/v1`,
   withCredentials: true,
 });
