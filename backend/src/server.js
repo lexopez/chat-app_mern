880f41b50c2ae3466ec247fb8d347e9f532f7754
alexander lopez <iamlexlopez@gmail.com>
2026-05-20 13:26:34 +0800

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,12 +21,13 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.ORIGIN || "http://localhost:5173",
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: process.env.ORIGIN || "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
+app.use(cors());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
