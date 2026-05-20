32783e09eca644d40a8579c8855dc08ddaf0c1be
alexander lopez <iamlexlopez@gmail.com>
2026-05-20 14:46:39 +0800

    update login page 2

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 import { Link } from "react-router-dom";
-import AuthImagePattern from "../components/AuthImagePattern";
+// import AuthImagePattern from "../components/AuthImagePattern";
 
 function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
