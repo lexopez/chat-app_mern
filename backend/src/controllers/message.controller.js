ae5c2f52c09e8b9f8975a6f21af5efaf412e9f3c
alexander lopez <iamlexlopez@gmail.com>
2026-05-26 08:57:44 +0800

    messages controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -16,24 +16,24 @@ export const getUsersForSidebar = async (req, res) => {
   }
 };
 
-export const getMessages = async (req, res) => {
-  try {
-    const { id: userToChatId } = req.params;
-    const myId = req.user._id;
+// export const getMessages = async (req, res) => {
+//   try {
+//     const { id: userToChatId } = req.params;
+//     const myId = req.user._id;
 
-    const messages = await Message.find({
-      $or: [
-        { senderId: myId, receiverId: userToChatId },
-        { senderId: userToChatId, receiverId: myId },
-      ],
-    });
+//     const messages = await Message.find({
+//       $or: [
+//         { senderId: myId, receiverId: userToChatId },
+//         { senderId: userToChatId, receiverId: myId },
+//       ],
+//     });
 
-    res.status(200).json(messages);
-  } catch (error) {
-    console.log("Error in getMessages controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
+//     res.status(200).json(messages);
+//   } catch (error) {
+//     console.log("Error in getMessages controller", error.message);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// };
 
 export const sendMessage = async (req, res) => {
   try {
